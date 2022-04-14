ee168e1c17c340809f5c8103cfbb5ca814d8b603
Brian Scaturro <scaturrob@gmail.com>
2022-04-14 10:16:00 -0400

    first commit

---
+++ backend/tests/Feature/TasksTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TaskTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_the_application_returns_a_successful_response()
+    {
+        $response = $this->get('/tasks');
+
+        $response->assertStatus(200);
+    }
+}
